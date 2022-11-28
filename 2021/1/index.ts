dee90f6df0ab5a7532f9d392c85bcb817ede74b5
Rohan Godha <rgodha24@brophybroncos.org>
2022-11-28 16:38:33 -0700

    2021 1

---
+++ 2021/1/index.ts
@@ -0,0 +1,38 @@
+export function part1(input: string) {
+  const data = input.split("\n");
+
+  return (
+    data
+      .map((x, index, array) => {
+        const now = parseInt(x);
+        const prev = parseInt(array[index - 1]) || 0;
+
+        return now > prev;
+      })
+      // return only trues
+      .filter((x) => x).length - 1
+  );
+}
+
+export function part2(input: string) {
+  const data = input.split("\n");
+  return (
+    data
+      .map((x, index, array) => {
+        const now = [parseInt(x), parseInt(array[index + 1]), parseInt(array[index + 2])].reduce((a, b) => a + b, 0);
+        // sum array
+
+        const prev = [
+          parseInt(array[index + 1]) || 0,
+          parseInt(array[index + 2]) || 0,
+          parseInt(array[index + 3]) || 0,
+        ].reduce((a, b) => a + b, 0);
+
+        // console.log(now, prev);
+
+        return now < prev;
+      })
+      // return only trues
+      .filter((x) => x).length 
+  );
+}

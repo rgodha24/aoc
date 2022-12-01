0ac01e645ca68d22a394dcf06f7a35f834f60954
Rohan Godha <rgodha24@brophybroncos.org>
2022-12-01 10:43:18 -0700

    2021 8 pt 1

---
+++ 2021/8/index.ts
@@ -0,0 +1,21 @@
+import * as utils from "utils";
+import { z } from "zod";
+
+export function part1(input: string) {
+  const data = z
+    .array(z.tuple([z.string().array(), z.string().array()]))
+    .parse(utils.splitByLines(input).map((line) => line.split(" | ").map((block) => block.split(" "))));
+
+  const answer = data.map(([a, b]) => {
+    // console.log(b.map((x) => x.length));
+    return b.map((c) => c.length === 2 || c.length === 4 || c.length === 3 || c.length === 7).filter((d) => d).length;
+  });
+
+  // console.log(answer);
+
+  return utils.sum(answer);
+}
+
+export function part2(input: string) {
+  return;
+}
